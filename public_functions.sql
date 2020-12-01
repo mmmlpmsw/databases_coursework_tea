bebfdc5f112ef8fd7347ff7eb71695e87b4c448a
david-d25@yandex.ru <david-d25@yandex.ru>
2020-12-01 21:01:36 +0300

    Fixed create_tea_composition floating point calculation errors

--- public_functions.sql
+++ public_functions.sql
@@ -3,7 +3,6 @@
   *
   * to be done:
   * 1) create_order - создает заказ, выбирает любой свободный грузовик(и)
-  * 2) выводить содержимое новой композиции
   *
  */
 
@@ -29,8 +28,8 @@ $$ language plpgsql;
 
 create or replace function create_tea_composition(
     name varchar,
-    description text)
-returns integer as $$
+    description text
+) returns integer as $$
 declare
     sum real := 0;
     tea_share real := random();
@@ -58,93 +57,38 @@ begin
        sum += percentage;
     }
  */
-    insert into tea_composition(created, name, description) values (now(), name, description) returning super_id into _composition_id;
+    insert into tea_composition(created, name, description)
+        values (now(), name, description)
+        returning super_id into _composition_id;
 
-    percentage := round(random()/5*100)/100;
-    insert into composition_item(composition_id, product_id, amount_percent) values (_composition_id, get_random_tea(), percentage);
+    percentage := 1.0/128 + round(random()/2*128)/128;
+    insert into composition_item(composition_id, product_id, amount_percent)
+        values (_composition_id, get_random_tea(), percentage);
     sum := sum + percentage;
 
     while sum < 1 loop
-        percentage := round(random()/3*100)/100;
-        if sum + percentage > 1 then
+        percentage := 1.0/128 + round(random()/2*128)/128;
+
+        if sum + percentage > (1 - 1.0/64) then
             percentage := 1 - sum;
         end if;
 
         if random() < tea_share then
-            insert into composition_item(composition_id, product_id, amount_percent) values (_composition_id, get_random_tea(), percentage) on conflict do nothing;
+            insert into composition_item(composition_id, product_id, amount_percent)
+                values (_composition_id, get_random_tea(), percentage) on conflict do nothing;
         else
-            insert into composition_item(composition_id, product_id, amount_percent) values (_composition_id, get_random_product(), percentage) on conflict do nothing;
+            insert into composition_item(composition_id, product_id, amount_percent)
+                values (_composition_id, get_random_product(), percentage) on conflict do nothing;
         end if;
 
-        select sum(amount_percent) from composition_item where composition_item.composition_id = _composition_id into sum;
+        sum := sum + percentage;
     end loop;
     return _composition_id;
 end
 $$ language plpgSQL;
 
---------------------------------------------
--- на всякий случай - как вариант использования?
--- create or replace function get_new_circuit_board_machine(cbmodels_count integer) returns integer as
--- $$
---     declare
---         new_machine_id integer := 0;
---         i integer := 0;
---         random_speed integer := 0;
---         employees_count integer := 0;
---         employee_id integer := 0;
---         circuit_board_model_row circuit_board_model%ROWTYPE;
---     begin
---         insert into circuit_board_machine(assembly_date, work_hrs, area, state) values (now(), 0, (random() * 100 + 1)::real, 'ok') returning id into new_machine_id;
---         for i in 1..cbmodels_count loop
---             select id, version into circuit_board_model_row from circuit_board_model order by id limit 1 offset i;
---             random_speed := (random() * ( 100 - 10) + 10)::real;
---             insert into circuit_board_machine_param_item values (machine_id, circuit_board_model_row.id, circuit_board_model_row.version, random_speed);
---         end loop;
---         select count(*) from factory_employee into employees_count;
---         for i in 1..employees_count loop
---             select id from factory_employee order by random() limit 1 into employee_id;
---             insert into employee_machine_xref values (employee_id, machine_id) on conflict do nothing;
---         end loop;
---     end;
--- $$ language plpgsql;
-
---------------------------------------------
--- на всякий случай - как вариант использования?
--- create or replace function buy_product(_store_id integer, _product_id integer, _amount real) returns void as
--- $$
--- declare
---     fact_amount real := 0;
--- begin
---     select amount from store_item where store_id = _store_id and product_id = _product_id into fact_amount;
---     if (_amount > fact_amount) then
---          return;
---     else
---         update store_item set amount = fact_amount - _amount where store_id = _store_id and product_id = _product_id;
---     end if;
--- end
--- $$ language plpgsql;
-
---------------------------------------------
--- на всякий случай- как вариант использования?
--- create or replace function add_tea_to_cupboard(_tea_id integer, _cupboard_id integer, _amount real) returns bool as
--- $$
--- declare
---     fact_amount real := 0;
---     tea_amount_in_cupboard real := 0;
---     tea_amount real := null;
--- begin
---     select sum(amount) from cupboard_item where cupboard_id = _cupboard_id into tea_amount_in_cupboard;
---     select amount from cupboard_item where cupboard_id = _cupboard_id and product_id = _tea_id into fact_amount;
---     if (_amount + tea_amount_in_cupboard > fact_amount) then
---          return false;
---     else
---         select amount from cupboard_item where product_id = _tea_id and cupboard_id = _cupboard_id into tea_amount;
---         if tea_amount is null then
---             insert into cupboard_item(product_id, cupboard_id, amount) values (_tea_id, _cupboard_id, _amount);
---         else
---             update cupboard_item set amount = _amount + tea_amount where cupboard_id = _cupboard_id and product_id = _tea_id;
---         end if;
---         return true;
---     end if;
--- end
--- $$ language plpgsql;
+create or replace function create_order() returns void as $$
+begin
+    -- todo
+end;
+$$ language plpgsql;
