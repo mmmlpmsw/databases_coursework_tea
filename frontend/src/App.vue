f49dfe283a679bf6cbb06fde1f7c1f9b7c06b174
david-d25@yandex.ru <david-d25@yandex.ru>
2020-12-19 04:01:52 +0300

    Added router, vuex, i18n and cleaned up cheats code

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,25 +1,80 @@
 <template>
-<!--  todo header-->
-<!--  todo vue-router-->
-
-  <snake-game/>
-  <tetris-game/>
+  <div id="root">
+    <a-header/>
+    <router-view/>
+    <cheats/>
+  </div>
 </template>
 
 <script>
-  import SnakeGame from "./components/SnakeGame";
-  import TetrisGame from "./components/TetrisGame";
+  import Vue from "vue";
+  import VueRouter from "vue-router";
+  import VueI18n from "vue-i18n";
+  import Vuex from "vuex";
 
-  // todo init router
-  // todo init i18n
-  // todo make preloader
-  // todo make global state
+  import Header from "$src/components/Header";
+
+  import i18n_ru_RU from '$assets/i18n/ru_RU.js';
+  import i18n_en_US from '$assets/i18n/en_US.js';
+  import Cheats from "$src/components/Cheats";
+
+  Vue.use(VueRouter);
+  Vue.use(Vuex);
+
+  let router = initRouter();
+  let i18n = initI18n();
+  let store = initGlobalStore();
+
+  function initRouter() {
+    const routes = [
+      { path: '/',        redirect: '/factory'  },
+      { path: '/stores',  component: null       }, // todo
+      { path: '/factory', component: null       }, // todo
+      { path: '/orders',  component: null       }, // todo
+    ];
+
+    return new VueRouter({
+      mode: 'history',
+      routes
+    });
+  }
+
+  function initI18n() {
+    let messages = {
+      'en_US': i18n_en_US,
+      'en': i18n_en_US,
+      'ru_RU': i18n_ru_RU,
+      'ru': i18n_ru_RU
+    };
+
+    let locale = 'en_US';
+
+    return new VueI18n({
+      locale: locale,
+      fallbackLocale: 'en_US',
+      messages
+    });
+  }
+
+  function initGlobalStore() {
+    return new Vuex.Store({
+      state: {
+        // Empty for now
+      },
+      mutations: {
+        // Empty for now
+      }
+    });
+  }
 
   export default {
     components: {
-      SnakeGame,
-      TetrisGame
-    }
+      Cheats,
+      'a-header': Header
+    },
+    router,
+    store,
+    i18n,
   }
 </script>
 
---
+++ frontend/src/components/Cheats.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <snake-game/>
+    <tetris-game/>
+  </div>
+</template>
+
+<script>
+  import SnakeGame from "$src/components/SnakeGame";
+  import TetrisGame from "$src/components/TetrisGame";
+
+  export default {
+    comments: [
+      SnakeGame,
+      TetrisGame
+    ]
+  }
+</script>
