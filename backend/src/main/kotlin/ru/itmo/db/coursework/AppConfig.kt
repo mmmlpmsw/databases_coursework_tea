7a95e423adacbb51e4c3c1cb5b5e36016cf1abb6
david-d25@yandex.ru <david-d25@yandex.ru>
2021-01-12 18:43:09 +0300

    Minimal working spring config

--- backend/src/main/kotlin/ru/itmo/db/coursework/AppConfig.kt
+++ backend/src/main/kotlin/ru/itmo/db/coursework/AppConfig.kt
@@ -1,12 +1,20 @@
 package ru.itmo.db.coursework
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.PropertySource
+import org.springframework.core.env.Environment
+import org.springframework.core.env.StandardEnvironment
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
 
 @EnableWebMvc
 @Configuration
+@PropertySource("classpath:app.properties")
 @ComponentScan("ru.itmo.db.coursework")
 open class AppConfig {
+    @Autowired
+    val env: Environment = StandardEnvironment()
+
 
 }
---
+++ backend/src/main/kotlin/ru/itmo/db/coursework/api/controller/TestController.kt
@@ -0,0 +1,15 @@
+package ru.itmo.db.coursework.api.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.util.*
+
+@RestController
+@RequestMapping("/test")
+class TestController {
+    @GetMapping
+    fun test(): String {
+        return "Hello! Today is ${Date()}"
+    }
+}
