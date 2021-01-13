7b902725709a9e5f371a07151512658f38165d9f
david-d25@yandex.ru <david-d25@yandex.ru>
2021-01-13 08:49:53 +0300

    Added icons for mover buttons

--- frontend/src/game/area/thing/moving/AreaThingMoverApplyButton.ts
+++ frontend/src/game/area/thing/moving/AreaThingMoverApplyButton.ts
@@ -2,10 +2,7 @@ import CommonAreaHudButton from "$src/game/area/CommonAreaHudButton";
 
 const BUTTON_SIZE = 40;
 const IMAGE_SIZE = 30;
-// todo
-const IMAGE_PATH = new Path2D(`
-
-`);
+const IMAGE_PATH = new Path2D(`M0 .6l.3 .3l.7 -.7l-.1 -.1l-.6 .6l-.2 -.2l-.1 .1`);
 const DEFAULT_COLOR = "rgb(39,204,61)";
 const HOVER_COLOR = "rgb(90,230,108)";
 const ACTIVE_COLOR = "rgb(32,153,46)";
--- frontend/src/game/area/thing/moving/AreaThingMoverCancelButton.ts
+++ frontend/src/game/area/thing/moving/AreaThingMoverCancelButton.ts
@@ -2,10 +2,7 @@ import CommonAreaHudButton from "$src/game/area/CommonAreaHudButton";
 
 const BUTTON_SIZE = 40;
 const IMAGE_SIZE = 30;
-// todo
-const IMAGE_PATH = new Path2D(`
-
-`);
+const IMAGE_PATH = new Path2D(`M.2 .1l.3 .3l.3 -.3l.1 .1l-.3 .3l.3 .3l-.1 .1l-.3 -.3l-.3 .3l-.1 -.1l.3 -.3l-.3 -.3`);
 const DEFAULT_COLOR = "rgb(204,67,37)";
 const HOVER_COLOR = "rgb(230,115,90)";
 const ACTIVE_COLOR = "rgb(153,57,31)";
